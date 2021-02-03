f7c9acaa8535ff6d8e12a017e037dd8167daf6c9
kasper-jensen <kasper.jensen@visma.com>
2021-02-03 11:01:15 +0100

    comments etc

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
-## OBS: just to check validations - on provider - so no variables are created
+## OBS: just to check validations - on provider - so no variables are created, thus it should fail on actually creating anything.
 resource "acme_certificate" "certificate" {
+  ## This provider comes with server_url set outside - if we remove server_url from the provider here in providers.tf it fails on validation
   provider                  = acme.provider_alias
   account_key_pem           = acme_registration.key
   common_name               = var.name
--- providers.tf
+++ providers.tf
@@ -1,4 +1,5 @@
 provider "acme" {
   alias      = "provider_alias"
+  #server_url should not be set here. since we get it from the top layer
   server_url = var.server_url
 }
